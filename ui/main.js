5a4a0bc67a14ea73b5bbe51b74af65f285cb4308
kiranmai15 <rachagiri.kiranmai@gmail.com>
2016-10-18 23:37:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,10 @@
 //Counter code
-var button=document.getElementById('counter');
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+};
 
-button.onclick=function(){
-    // create a request
-    var request = new XMLHttpRequest();
-    //Capture the response and store it in a variable
-    request.onreadyStateChange=function(){
-      if(request.readyState === XMLHttpRequest.DONE){
-          //take some action
-          if(request.status === 200){
-             var counter = request.responseText;
-              var span=document.getElementById('count');
-              span.innerHTML=counter.toString();
-          }
-      }
-      //Not done yet
-      }  
-    };
-    //Make a request
-    request.open('GET',"http://kiranmai15.imad.hasura-app.io/counter",true);
-    request.send(null);
-  };
