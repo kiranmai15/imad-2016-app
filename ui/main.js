76542087a7a90cbce06fde3825bcc67354b32791
kiranmai15 <rachagiri.kiranmai@gmail.com>
2016-10-19 12:24:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,13 +25,26 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    //Make a request to server and send the name
+   
+    // create a request
+    var request = new XMLHttpRequest();
     //capture a list of names and render them as a list
-    var names=['name1','name2','name3', 'name4'];
-    var list='';
-    for (var i=0; i< names.length; i++){
-        list += '<li>' + names[i] + '<li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML = list;
+     request.onreadystatechange=function(){
+          if(request.readyState === XMLHttpRequest.DONE){
+               //take some action
+          if(request.status === 200){
+              var names=['name1','name2','name3', 'name4'];
+              var list='';
+              for (var i=0; i< names.length; i++){
+              list += '<li>' + names[i] + '<li>';
+              }
+              var ul=document.getElementById('namelist');
+              ul.innerHTML = list;
+          }
+          }
+     };
+       //Make a request to server and send the name
+    request.open('GET',"http://kiranmai15.imad.hasura-app.io/submit-name?name=",name,true);
+    request.send(null);
+          
 };
