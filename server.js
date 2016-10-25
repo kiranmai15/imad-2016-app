3407302160a7666bf818dffc2b5629ff48951b92
kiranmai15 <rachagiri.kiranmai@gmail.com>
2016-10-25 21:09:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path'); 
+var Pool= require('pg').Pool;
 
-
-
+var config= {
+    user: 'kiranmai15',
+    database: 'kiranmai15',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+    
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -12,6 +19,20 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool= new pool('config');
+app.get('/test-db', function(req, res){
+   //make a select request
+   //return the response with results
+   pool.query('SELECT * FROM test', function(err,result){
+       if(err){
+           res.status(500).send(err,toString());
+       }else{
+           res.send(JSON.stringify(result));
+       }
+   });
+});
+
 var counter=0;
 app.get('/counter',function(req, res){
     counter=counter+1;
