57c2573a61d9bc5f714c5a30dcf58463af7f202a
kiranmai15 <rachagiri.kiranmai@gmail.com>
2016-10-25 20:29:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config= {
     user: 'kiranmai15',
     database: 'kiranmai15',
     host: 'db.imad.hasura-app.io',
-    port: '5432'
+    port: '5432',
     password: process.env.DB_PASSWORD
     
 };
