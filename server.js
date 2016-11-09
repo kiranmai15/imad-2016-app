c9583028685af070c556316c13fd5ca919a8787d
kiranmai15 <rachagiri.kiranmai@gmail.com>
2016-11-09 14:07:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,6 +35,9 @@ app.get('/article-three', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/article-one.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.css'));
+});
 app.get('/ui/main.js',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
