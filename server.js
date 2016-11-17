a8abb878dc61937eabcb5f8462ad27d5779f59e5
kiranmai15 <rachagiri.kiranmai@gmail.com>
2016-11-17 20:32:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,12 +25,6 @@ app.get('/index', function (req, res){
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -41,9 +35,7 @@ app.get('/ui/article-one.css', function (req, res) {
 app.get('/ui/main.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.css'));
 });
-app.get('/ui/article-three.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.css'));
-});
+
 app.get('/ui/jquery-1.4.2.js',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'jquery-1.4.2.js'));
 });
