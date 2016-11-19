93d05863f32ebac1a898662d3bfc9c8f1daf42c4
kiranmai15 <rachagiri.kiranmai@gmail.com>
2016-11-19 11:15:22 +0530

    [imad-console] Updates ui/login.js

--- ui/login.js
+++ ui/login.js
@@ -89,6 +89,25 @@ function loadLoggedInUser (username) {
         <a href="/logout">Logout</a>
     `;
     }
+ function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
 
 function loadLogin () {
     // Check if the user is already logged in
