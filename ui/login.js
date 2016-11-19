fe5b0ad20ba7f803bf80d57a45318a525ac96a69
kiranmai15 <rachagiri.kiranmai@gmail.com>
2016-11-19 16:36:28 +0530

    [imad-console] Updates ui/login.js

--- ui/login.js
+++ ui/login.js
@@ -7,7 +7,7 @@ function signupElement(){
 }
 function loadLoginForm () {
     var loginHtml = `
-    <h3>Login/Register to unlock awesome features</h3>
+    
         <input type="text" id="username" placeholder="username" />
         <input type="password" id="password" />
         <br/><br/>
