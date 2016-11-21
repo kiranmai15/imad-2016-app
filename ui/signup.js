08fd11c455deb83f248eead7cee79700ef229073
kiranmai15 <rachagiri.kiranmai@gmail.com>
2016-11-21 15:48:07 +0530

    [imad-console] Updates ui/signup.js

--- ui/signup.js
+++ ui/signup.js
@@ -2,7 +2,17 @@ function signupForm(){
   
      var ni = document.getElementById('register_area');
      var newdiv = document.createElement('div');
-      newdiv.innerHTML = `Username:<br>
+      newdiv.innerHTML = `
+       Username:<br>
+        <input type="text" id="firstname" placeholder="enter your name" />
+        <br>
+         Username:<br>
+        <input type="text" id="lastname" placeholder="enter your surname" />
+        <br>
+         Username:<br>
+        <input type="text" id="email" placeholder="email@gmail.com" />
+        <br>
+      Username:<br>
         <input type="text" id="username" placeholder="username" />
         <br>
         Password:<br>
@@ -15,7 +25,6 @@ function signupForm(){
 
  var register = document.getElementById('register_btn');
     register.onclick = function () {
-        alert('about to register');
         // Create a request object
         var request = new XMLHttpRequest();
         
@@ -36,11 +45,17 @@ function signupForm(){
         // Make the request
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
+        var firstname = document.getElementById('firstname').value;
+        var lastname = document.getElementById('lastname').value;
+        var email = document.getElementById('email').value;
         console.log(username);
         console.log(password);
+        console.log(firstname);
+         console.log(lastname);
+        console.log(email);
         request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
+        request.send(JSON.stringify({username: username, password: password, firstname: firstname, lastname: lastname, email: email}));  
         register.value = 'Registering...';
     
 };
