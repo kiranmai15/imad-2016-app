b08a4a9c7df9d1e3b5217ea136156f468e60abb4
kiranmai15 <rachagiri.kiranmai@gmail.com>
2016-11-21 15:37:39 +0530

    [imad-console] Updates ui/login.js

--- ui/login.js
+++ ui/login.js
@@ -7,7 +7,7 @@ function loadLoginForm () {
         <input type="password" id="password" />
         <br/><br/>
        <input type="submit" id="login_btn" value="Login" />
-      <input type="submit" id="register_btn" value="Register" />
+      
         `;
   
    
@@ -53,37 +53,7 @@ function loadLoginForm () {
         submit.value = 'Logging in...';
         
     };
-    var register = document.getElementById('register_btn');
-    register.onclick = function () {
-        alert('about to register');
-        // Create a request object
-        var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
-              if (request.status === 200) {
-                  alert('User created successfully');
-                  register.value = 'Registered!';
-              } else {
-                  alert('Could not register the user');
-                  register.value = 'Register';
-              }
-          }
-        };
-        
-        // Make the request
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
-        request.open('POST', '/create-user', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
-        register.value = 'Registering...';
-    
-};
+   
     
    
 }
