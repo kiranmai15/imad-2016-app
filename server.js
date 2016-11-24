3db12d554f8bc19a94a49a2c17d4d5f0849189e3
kiranmai15 <rachagiri.kiranmai@gmail.com>
2016-11-24 22:44:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,9 +98,17 @@ app.post('/login', function (req, res) {
    });
 });
 app.get('/check-login', function (req, res) {
-if (req.session && req.session.auth && req.session.auth.userId) {
-    res.sendFile(path.join(__dirname, 'ui', 'courses.html'));
-    res.send("You are not logged in");
+ if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
 }
 
 });
