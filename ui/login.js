5450aa9b9d8c6159df0c8d4c8f4a8dc3adeabf1f
kiranmai15 <rachagiri.kiranmai@gmail.com>
2016-11-25 17:02:46 +0530

    [imad-console] Updates ui/login.js

--- ui/login.js
+++ ui/login.js
@@ -81,7 +81,7 @@ function loadLoggedInUser (username) {
   <button onclick="myFunction()" class="dropbtn">Select a course</button>
   <div id="myDropdown" class="dropdown-content">
     <a href="http://kiranmai15.imad.hasura-app.io/courses">Core Java</a>
-    <a href="#">Operating Systems</a>
+    <a href="#">Linux</a>
   </div>
 </div>
        
