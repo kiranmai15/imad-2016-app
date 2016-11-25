a112b77a292f72a093d640bb32b66b57fb743b22
kiranmai15 <rachagiri.kiranmai@gmail.com>
2016-11-25 17:07:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.get('/check-login', function (req, res) {
 });
 app.get('/logout', function (req, res) {
    delete req.session.auth;
-   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to Home</a></body></html>');
+   res.sendFile(path.join(__dirname, 'ui', 'main.html'));
 });
 app.get('/index',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
